b08cb9a22f1d6f69bc3ae576f6d444b05f8b61c9
dbowring <danieltbowring@gmail.com>
2013-04-08 19:40:44 +1000

    Add UI.select_folder @thomthom

---
+++ ui.rb
@@ -0,0 +1,10 @@
+module UI
+    def self.select_folder(title=nil, initial=nil)
+        # TODO: default title to localised string
+        if initial.nil? || !File.directory?(initial)
+            initial = Sketchup.find_support_file('')
+        end
+        # Show UI element to select folder
+        raise NotImplemented()
+    end
+end
